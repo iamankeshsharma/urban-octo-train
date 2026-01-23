e9a80c9cb3889384af19ad2bd33d5ce1f7619d95
ANKESH <ankeshsharma@protonmail.com>
2026-01-23 11:25:03 +0530

    feat: Added color schema in tailwind config alongwith favicon icon and font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Space Mono"', 'monospace'],
+      },
+      colors: {
+        primary: '#1E1E1E',
+        secondary: '#636363',
+        'accent-green': '#01F701',
+        'accent-blue': '#7F8AF4',
+        'accent-yellow': '#F3F303',
+      },
+    },
   },
   plugins: [],
 }
