0cec9b28eccf5a0c977fc3134d4b06d7264aa077
ANKESH <ankeshsharma@protonmail.com>
2026-01-28 18:22:32 +0530

    fix: navigation bg color is set and z-index of experience card droped to initial.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-neutral-900 text-white p-4">
-      <NavigationBar links={navLinks} className="fixed top-0 left-0 right-0 z-50" />
+      <NavigationBar links={navLinks} className="bg-neutral-900 fixed top-0 left-0 right-0 z-50" />
       <HeroSection />
       <ExperienceSection data={experiences} />
     </div >
--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -11,7 +11,7 @@ type props = {
 
 const ExperienceCard = ({ data, className, children, position = 'left' }: props) => {
     return (
-        <div className={twMerge("relative w-full border border-secondary z-50 py-2 px-4 rounded-md ring ring-white/10", className)}>
+        <div className={twMerge("relative w-full border border-secondary py-2 px-4 rounded-md ring ring-white/10", className)}>
             {children}
             <div className={twMerge("hidden lg:flex absolute top-1/2 -translate-y-1/2 items-center", position === 'left' ? '-left-[50px]' : '-right-[50px]')}>
                 <span className="bg-white size-4 rounded-full border" />
